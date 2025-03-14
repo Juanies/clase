fabfb72f895518f9932eeb96e1a748b72cc4fa6b
juanies <juanromeroviudez@gmail.com>
2025-03-14 13:31:29 +0100

    Combina2

--- laravel/resources/views/contacto.blade.php
+++ laravel/resources/views/contacto.blade.php
@@ -1,2 +1,18 @@
 <!-- He creado la vista contacto -->
 naubsu
+<awdawdawwd>awd
+    aw
+    dawdẃadáw´dẃ<address><i class="fa fa-window-closeaw
+        da
+        wd
+        wad
+        aw
+        dẃafñ,.ea fñéałf
+        F as FC.
+        AQwf :ASf:Dawsf: as:f A
+        ´FW, sc:Awf ;:SC.fef
+         SD:CQWEO3ÑP0LIKUJYHGNFBV" aria-hidden="true"></i></address>
+</awdawdawwd>KIJUYHGVBHNJHUYGTFCV BNJHGFCD BNJHGV
+<aw></aw>
+<d></d>
+<awGIT></awGIT 
